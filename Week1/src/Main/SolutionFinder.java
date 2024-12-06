479509bbd3366bd0b1fa5c713d7b2d911fdf954c
FineNlow <thanabodee.nine@gmail.com>
2024-12-06 08:53:00 +0700

    new Lab01, 02 not complete yet

---
+++ Week1/src/Main/SolutionFinder.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author User
+ */
+public class SolutionFinder {
+    public static void main(String[] args) {
+        double a = 4, b = 8, c = 3;
+        System.out.println(((b*-1) + (Math.sqrt(Math.pow(b, 2) - (4*a*c)))) / (2*a));
+        System.out.println(((b*-1) - (Math.sqrt(Math.pow(b, 2) - (4*a*c)))) / (2*a));
+    }
+}
---
+++ Week2/src/week2/Salary.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2;
+import java.util.Scanner;
+/**
+ *
+ * @author User
+ */
+public class Salary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please insert your name : ");
+        String name = input.nextLine();
+        System.out.println("");
+    }
+}
